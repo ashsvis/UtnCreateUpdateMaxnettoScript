0383b9fac4452eab7c82cac2b17f097e48046fd3
ashsvis <ashsvis@gmail.com>
2020-12-07 12:43:18 +0400

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace UtnCreateUpdateMaxnettoScript
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.lvTable = new System.Windows.Forms.ListView();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(14, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 26);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Загрузить таблицу...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(14, 46);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(825, 395);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.lvTable);
+            this.tabPage1.Location = new System.Drawing.Point(4, 24);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(817, 367);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Исходная таблица";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // lvTable
+            // 
+            this.lvTable.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lvTable.FullRowSelect = true;
+            this.lvTable.Location = new System.Drawing.Point(7, 7);
+            this.lvTable.Name = "lvTable";
+            this.lvTable.ShowItemToolTips = true;
+            this.lvTable.Size = new System.Drawing.Size(801, 349);
+            this.lvTable.TabIndex = 0;
+            this.lvTable.UseCompatibleStateImageBehavior = false;
+            this.lvTable.View = System.Windows.Forms.View.Details;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 24);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(817, 346);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Результирующий скрипт";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "xls";
+            this.openFileDialog1.Filter = "*.xls|*.xls|*.xlsx|*.xlsх";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(853, 455);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds;
+            this.Text = "Создание скрипта на изменение MAXNETTO для вагонов";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.ListView lvTable;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UtnCreateUpdateMaxnettoScript
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                LoadExcelFile(openFileDialog1.FileName);
+            }
+        }
+
+        private void LoadExcelFile(string fileName)
+        {
+            Cursor = Cursors.WaitCursor;
+            dynamic xl = Activator.CreateInstance(Type.GetTypeFromProgID("Excel.Application"));
+            try
+            {
+                var wb = xl.Workbooks.Open(fileName, 0, true);
+                try
+                {
+                    var sheet = wb.Sheets[1];
+                    var xlCellTypeLastCell = 11;
+                    var lastCell = sheet.Cells.SpecialCells(xlCellTypeLastCell);
+                    var colIndexName = IndexToAbc(lastCell.Column);
+                    var arrData = (object[,])sheet.Range[$"A1:{colIndexName}{lastCell.Row}"].Value;
+                    var rowCount = arrData.GetLength(0);
+                    var columnCount = arrData.GetLength(1);
+                    using (var gr = lvTable.CreateGraphics())
+                    {
+                        var font = lvTable.Font;
+                        lvTable.Columns.Clear();
+                        float width = 0f;
+                        var dict = new Dictionary<string, float>();
+                        var names = new List<string> { "(stub)" };
+                        for (var col = 1; col <= columnCount; col++)
+                        {
+                            var colName = $"{arrData[2, col]}";
+                            names.Add(colName);
+                            width = gr.MeasureString(colName, font).Width;
+                            dict.Add(colName, width);
+                            lvTable.Columns.Add(colName);
+                        }
+                        lvTable.Items.Clear();
+                        for (var row = 3; row <= rowCount; row++)
+                        {
+                            var rowValue = $"{arrData[row, 1]}";
+                            if (DateTime.TryParse(rowValue, out DateTime dt))
+                                rowValue = $"{dt:dd.MM.yy HH:mm:ss}";
+                            width = gr.MeasureString(rowValue, font).Width;
+                            if (width > dict[names[1]]) dict[names[1]] = width;
+                            var lvi = new ListViewItem(rowValue);
+                            for (var col = 2; col <= columnCount; col++)
+                            {
+                                rowValue = $"{arrData[row, col]}";
+                                width = gr.MeasureString(rowValue, font).Width + 10f;
+                                if (width > dict[names[col]]) dict[names[col]] = width;
+                                lvi.SubItems.Add(rowValue);
+                            }
+                            lvTable.Items.Add(lvi);
+                        }
+                        for (var col = 0; col < lvTable.Columns.Count; col++)
+                        {
+                            var column = lvTable.Columns[col];
+                            column.Width = Convert.ToInt32(dict[names[col + 1]]);
+                        }
+                    }
+                }
+                finally
+                {
+                    wb.Close(false);
+                }
+            }
+            finally
+            {
+                xl.Quit();
+                Cursor = Cursors.Default;
+            }
+        }
+
+        private static string IndexToAbc(int index)
+        {
+            const string s = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            var letters = s.ToCharArray();
+            var result = "";
+            if (index <= letters.Length)
+            {
+                result = $"{letters[index - 1]}";
+                return result;
+            }
+            while (true)
+            {
+                var big = (index - 1) % 26;
+                result = $"{letters[big]}{result}";
+                index = (index - 1) / 26;
+                if (index <= letters.Length)
+                    return result;
+            }
+        }
+    }
+}
